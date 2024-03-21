def7bcad9b7c972cc4565d50374143b4d5fc66f4
Kristian <10221425@ucn.dk>
2024-03-21 14:12:37 +0100

    register & login done

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 
 Route::get('/home', function () {
